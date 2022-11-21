886b07342ac282099c1b5c9d124c98cbe476ac91
Mustang00 <97599183+Holodeec@users.noreply.github.com>
2022-11-21 14:02:49 +0300

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
@@ -9,13 +10,16 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        Util.getConnection();
-        UserService userService = new UserServiceImpl();
-//
-        userService.createUsersTable();
-//        //userService.removeUserById(5);
-       userService.saveUser("Alex","Djordan",(byte) 55);
-//        userService.getAllUsers();
+
+//        UserDao userDao = new UserDaoJDBCImpl();
+
+        UserDao userDao1 = new UserDaoHibernateImpl();
+//        userDao1.saveUser("Alex","Djordon",(byte) 33);
+//        userDao1.saveUser("Vertro","Getso",(byte) 23);
+//        userDao1.saveUser("РАБОТАЙ","ПОЖАЛУСТА",(byte) 43);
+        userDao1.dropUsersTable();
+        System.out.println(userDao1.getAllUsers());
+
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,101 @@
-//package jm.task.core.jdbc.dao;
-//
-//import jm.task.core.jdbc.model.User;
-//
-//import java.util.List;
-//
-//public class UserDaoHibernateImpl implements UserDao {
-//    public UserDaoHibernateImpl() {
-//
-//    }
-//
-//
-//    @Override
-//    public void createUsersTable() {
-//
-//    }
-//
-//    @Override
-//    public void dropUsersTable() {
-//
-//    }
-//
-//    @Override
-//    public void saveUser(String name, String lastName, byte age) {
-//
-//    }
-//
-//    @Override
-//    public void removeUserById(long id) {
-//
-//    }
-//
-//    @Override
-//    public List<User> getAllUsers() {
-//        return null;
-//    }
-//
-//    @Override
-//    public void cleanUsersTable() {
-//
-//    }
-//}
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoHibernateImpl implements UserDao {
+    public UserDaoHibernateImpl() {
+
+    }
+
+
+    @Override
+    public void createUsersTable() {
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE if not exists users(id int primary key auto_increment," +
+                    "name varchar(20) not null ,lastName varchar(20) not null ," +
+                    "age int)").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Еху!!!");
+        } catch (RuntimeException e) {
+            System.out.println("ФакОф...");
+        }
+
+    }
+
+    @Override
+    public void dropUsersTable() {
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("drop table if exists users").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Еху!!!");
+        } catch (RuntimeException e) {
+            System.out.println("ФакОф...");
+        }
+
+
+
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        try(Session session = Util.getSessionFactory().openSession()) {
+            User user = new User(name,lastName,age);
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            System.out.println("User с именем " + name + " добавлен в базу данных.");
+        } catch (RuntimeException e) {
+            System.out.println("Страдай еще...");
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete User where id="+id).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("User с id " + id +" удален.");
+
+        } catch (RuntimeException e) {
+            System.out.println("ФакОф...");
+        }
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> list = null;
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            list = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
+            System.out.println("Еху!!!");
+        } catch (RuntimeException e) {
+            System.out.println("ФакОф...");
+        }
+
+        return list;
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Таблица users очищена");
+        } catch (RuntimeException e) {
+            System.out.println("ФакОф...");
+        }
+    }
+}
